98f5ad6e987b12e698ac7334aa7cb08f39457c99
ITWebde <baxriddinovsamandar07@gmail.com>
2021-11-02 14:15:20 +0500

    Javascript information

---
+++ js/main.js
@@ -0,0 +1,39 @@
+var firstName = prompt('Ismingiz');
+alert(firstName);
+console.log(firstName);
+
+var family = prompt('Familyangiz');
+alert(family);
+console.log(family);
+
+var age = prompt('Yoshingiz');
+alert(age);
+console.log(age);
+
+var year = prompt('Yilingiz');
+alert(year);
+console.log(year);
+
+var birthday = prompt('Tug`ulgan kuningiz');
+alert(birthday);
+console.log(birthday);
+
+var placeStudy = prompt('O`qish joyingiz');
+alert(placeStudy);
+console.log(placeStudy);
+
+var howMuchTime = prompt('Qancha vaqt o`qisiz');
+alert(howMuchTime);
+console.log(howMuchTime);
+
+var whichSphere = prompt('Qaysi soha bo`yicha');
+alert(whichSphere);
+console.log(whichSphere);
+
+var confidence = prompt('Bu sohada o`zizga ishonasizmi');
+alert(confidence);
+console.log(confidence);
+
+var liking = prompt('O`zingizni sohangizni sevasizmi');
+alert(liking);
+console.log(liking);
